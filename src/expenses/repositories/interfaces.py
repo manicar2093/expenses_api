bf57f981f9d19516c8bf4d5065a1692fee1ae40d
Manuel Carbajal <manicar2093@gmail.com>
2022-07-02 22:42:37 -0500

    chore: Remove python impl

--- src/expenses/repositories/interfaces.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from src.entities import Expense
-
-
-class IExpensesRepository(ABC):
-    @abstractmethod
-    def save(self, expense: Expense):
-        raise NotImplementedError
