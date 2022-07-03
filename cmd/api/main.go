bb465794742ba6933fe1409ca961ccbe5011b9a8
Manuel Carbajal <manicar2093@gmail.com>
2022-07-03 17:58:14 -0500

    feat: Add create income route

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,23 +6,46 @@ import (
 	"github.com/labstack/echo"
 	"github.com/manicar2093/expenses_api/internal/connections"
 	"github.com/manicar2093/expenses_api/internal/expenses"
+	"github.com/manicar2093/expenses_api/internal/incomes"
 	"github.com/manicar2093/expenses_api/internal/repos"
 )
 
 var (
-	conn          = connections.GetRelConnection()
-	expensesRepo  = repos.NewExpensesRepositoryImpl(conn)
-	createExpense = expenses.NewCreateExpensesImpl(expensesRepo)
-	e             = echo.New()
+	conn = connections.GetRelConnection()
+	e    = echo.New()
 )
 
 func main() {
 	expensesRoutes()
+	incomesRouter()
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
+func incomesRouter() {
+	var (
+		incomesRepo  = repos.NewIncomesRepositoryImpl(conn)
+		createIncome = incomes.NewCreateIncomeImpl(incomesRepo)
+		incomesGroup = e.Group("/incomes")
+	)
+	incomesGroup.POST("", func(ctx echo.Context) error {
+		var incomeRequest incomes.CreateIncomeInput
+		if err := ctx.Bind(&incomeRequest); err != nil {
+			return err
+		}
+		newIncome, err := createIncome.Create(ctx.Request().Context(), &incomeRequest)
+		if err != nil {
+			return err
+		}
+		return ctx.JSON(http.StatusCreated, newIncome)
+	})
+}
+
 func expensesRoutes() {
-	expensesGroup := e.Group("/expenses")
+	var (
+		expensesRepo  = repos.NewExpensesRepositoryImpl(conn)
+		createExpense = expenses.NewCreateExpensesImpl(expensesRepo)
+		expensesGroup = e.Group("/expenses")
+	)
 	expensesGroup.POST("", func(ctx echo.Context) error {
 		var expenseRequest expenses.CreateExpenseInput
 		if err := ctx.Bind(&expenseRequest); err != nil {
