6cc31ca9659270924ab951ae84b60bd617fececc
Manuel Carbajal <manicar2093@gmail.com>
2022-10-18 15:11:33 -0500

    feat: add expenses repo find by interface id

--- pkg/converters/object_id.go
+++ pkg/converters/object_id.go
@@ -2,10 +2,19 @@ package converters
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-func TurnToObjectID(possibleID string) (primitive.ObjectID, error) {
-	gotID, err := primitive.ObjectIDFromHex(possibleID)
-	if err != nil {
-		return primitive.ObjectID{}, &IDNotValidIDError{}
+func TurnToObjectID(possibleID interface{}) (primitive.ObjectID, error) {
+	var (
+		emptyObjectID = primitive.ObjectID{}
+		notValidError = &IDNotValidIDError{}
+	)
+	switch possibleID := possibleID.(type) {
+	case string:
+		gotID, err := primitive.ObjectIDFromHex(possibleID)
+		if err != nil {
+			return emptyObjectID, notValidError
+		}
+		return gotID, nil
+	default:
+		return emptyObjectID, notValidError
 	}
-	return gotID, nil
 }
