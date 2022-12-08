423b39b0005184a138cfa39873a528202ef3bce1
Manuel Carbajal <manicar2093@gmail.com>
2022-12-08 01:01:33 -0600

    feat: add user table and entity

---
+++ internal/entities/user.go
@@ -0,0 +1,18 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gopkg.in/guregu/null.v4"
+)
+
+type User struct {
+	ID        uuid.UUID   `json:"id,omitempty" gorm:"primaryKey,->"`
+	Name      null.String `json:"name,omitempty"`
+	Lastname  null.String `json:"lastname,omitempty"`
+	Email     string      `json:"email,omitempty"`
+	Avatar    null.String `json:"avatar,omitempty"`
+	CreatedAt *time.Time  `json:"created_at,omitempty"`
+	UpdatedAt *time.Time  `json:"updated_at,omitempty"`
+}
